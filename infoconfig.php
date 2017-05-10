e5721dddb7cab67f669ee54fb96fa904feda4682
bruce <test11798@gmail.cm>
2017-05-10 15:09:38 +0800

    Initial commit

---
+++ infoconfig.php
@@ -0,0 +1,12 @@
+<?php
+ $infoconfig	= array (
+  'URL_MODEL' => 0,
+  'site_url' => 'https://zw.qiyeclub.cn',
+  'up_size' => '50000',
+  'up_exts' => 'mp4',
+  'up_path' => './data/upload',
+  'thumb' => '1',
+  'thumb_width' => '400',
+  'thumb_height' => '500',
+);
+?>
